166292eb55d0f72df336f6d13aacd81f04b10c28
Nazar <nazarc0@Nazars-MacBook-Air.local>
2026-07-18 14:35:39 +0300

    fix_oidc

--- 03-aws-ecs/iam-github.tf
+++ 03-aws-ecs/iam-github.tf
@@ -1,13 +1,16 @@
 # 1. Створюємо OIDC Провайдер для GitHub
-resource "aws_iam_openid_connect_provider" "github" {
-  url            = "https://token.actions.githubusercontent.com"
-  client_id_list = ["sts.amazonaws.com"]
+# 1. Автоматично стягуємо актуальний сертифікат із GitHub
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
 
-  # Офіційні thumbprints сертифікатів GitHub
-  thumbprint_list = [
-    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
-  ]
+# 2. Створюємо OIDC Провайдер із динамічним відбитком
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  
+  # Тепер Терраформ сам підставлятиме сюди правильний ключ
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
 }
 
 # 2. Роль для конвеєра з перевіркою репозиторію
